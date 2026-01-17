8cb7b6c1a86fe3075891926ad708e82b820f6385
Brian <xbrianmoore@gmail.com>
2026-01-16 23:19:35 -0500

    read 1-16

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,12 @@
 // this is your main.js script
+
+let number = prompt('Enter num', 'num goes here')
+console.log(number)
+
+const agree = confirm('Are you sure?')
+console.log(agree)
+
+const now = new Date()
+console.log(now)
+console.log(now.getFullYear)
+console.log(now.getMonth)
